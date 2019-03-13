82e4b3cfe0cb7e7dce874ee543d166f78a34430f
Unknown <moritz.lauper@hispeed.ch>
2019-03-13 14:36:26 +0100

    initialize spring boot

---
+++ src/main/java/ch/noseryoung/uk223/Uk223Application.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uk223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uk223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uk223Application.class, args);
+	}
+
+}
