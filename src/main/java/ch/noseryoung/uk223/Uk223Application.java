377f93f5ea330d901c5d3dbe23eda4bbd819e5f6
Unknown <moritz.lauper@hispeed.ch>
2019-03-14 09:56:49 +0100

    auto generate database
    generate the database automatically using the applications.properties file.

--- src/main/java/ch/noseryoung/uk223/Uk223Application.java
+++ src/main/java/ch/noseryoung/uk223/Uk223Application.java
@@ -2,8 +2,10 @@ package ch.noseryoung.uk223;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Uk223Application {
 
 	public static void main(String[] args) {
